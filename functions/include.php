22c35c34fcf2023fd09091d190ebe1826aa306df
PowerChaos <PowerChaos@users.noreply.github.com>
2017-01-05 13:47:45 +0100

    Fixed Session Errors and locations

--- functions/include.php
+++ functions/include.php
@@ -1,6 +1,6 @@
 <?php
+//require_once (getenv("DOCUMENT_ROOT")."/functions/sessions.php");
 require_once (getenv("DOCUMENT_ROOT")."/functions/database.php");
-require_once (getenv("DOCUMENT_ROOT")."/functions/sessions.php");
 require_once (getenv("DOCUMENT_ROOT")."/functions/pages.php");
 require_once (getenv("DOCUMENT_ROOT")."/functions/permissions.php");
 require_once (getenv("DOCUMENT_ROOT")."/functions/PasswordStorage.php");
